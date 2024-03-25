23c9c75cc7c94dc7f68f3868db5395d6265e0b8b
Neil Albert Dela Cruz <143504592+WH20ter21@users.noreply.github.com>
2024-03-25 17:15:21 +0800

    Update route.js

--- src/app/api/categories/route.js
+++ src/app/api/categories/route.js
@@ -1,23 +1,19 @@
 import { Category } from "../../../models/Category";
 import mongoose from "mongoose";
-import { isAdmin } from "../auth/[...nextauth]/route";
 
 export async function POST(req) {
     mongoose.connect(process.env.MONGO_URL);
     const {name} = await req.json();
-    if (await isAdmin()) {
+     
         const categoryDoc = await Category.create({name});
         return Response.json(categoryDoc);
-    } else {
-        return Response.json({});
-    }
 }
 export async function PUT(req) {
     mongoose.connect(process.env.MONGO_URL);
     const {_id, name} = await req.json();
-    if (await isAdmin()) {
-        await Category.updateOne({_id}, {name});
-    }
+  
+    await Category.updateOne({_id}, {name});
+    
     return Response.json(true);
 }
 
@@ -32,8 +28,8 @@ export async function DELETE(req) {
     mongoose.connect(process.env.MONGO_URL);
     const url = new URL(req.url);
     const _id = url.searchParams.get('_id');
-        if (await isAdmin()) {
-            await Category.deleteOne({_id});
-        }
+        
+    await Category.deleteOne({_id});
+        
     return Response.json(true);
 }
