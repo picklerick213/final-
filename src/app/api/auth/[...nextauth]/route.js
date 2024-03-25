810de06bc7fc0007c54d38d78973b885774f56cf
Neil Albert Dela Cruz <143504592+WH20ter21@users.noreply.github.com>
2024-03-25 15:19:29 +0800

    Update route.js

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -5,7 +5,7 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import clientPromise from "@/libs/mongoConnect";
-import { isAdmin } from "./isAdmin"; // Import isAdmin function
+import { isAdmin } from "@/app/route"; // Import isAdmin function
 
 export const authOptions = {
   secret: process.env.SECRET,
